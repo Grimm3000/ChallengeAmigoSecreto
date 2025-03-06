b3403a2fbf430c0321ed815d68fd2913a25fa456
Grima Romero <grimarf@hotmail.com>
2025-03-05 19:00:00 -0500

    Crea el PROGRAMA

---
+++ app.js
@@ -0,0 +1,41 @@
+
+//Arreglos y variables
+const entradaAmigo = document.getElementById("amigo");
+const amigos =[];
+const listaAmigos = document.getElementById("listaAmigos");
+const amigoSecret = document.getElementById("resultado");
+
+console.log(listaAmigos.value);
+// listaAmigos.value
+listaAmigos[0]="Sabrina";
+
+//Funciones
+
+function agregarAmigo(){
+    if(entradaAmigo.value ==""){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        
+    amigos.push(entradaAmigo.value);
+    console.log(amigos);
+    listaAmigos.innerHTML =  listaAmigos.innerHTML + `<li>${entradaAmigo.value}</li>`;
+
+    }
+    entradaAmigo.value = "";
+}
+
+function sortearAmigo(){
+    if (amigos.length == 0) {
+        alert("No hay amigos en la lista para sortear.");
+       
+    }else{
+    let numRandom = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[numRandom];
+    
+    amigoSecret.innerHTML = `${amigoSecreto}`;
+    }
+
+   
+
+
+}
